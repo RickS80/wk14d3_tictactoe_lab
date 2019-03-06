f205805622318f70e476581a0f7dfb2ba6efb505
Rick Symington <rick.symington@gmail.com>
2019-03-06 14:58:32 +0000

    Added clear board

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,36 +8,70 @@ constructor(props){
     this.state = {
         gameObject: {
             cell1: "", 
-            cell2: "X",
-            cell3: "O", 
+            cell2: "",
+            cell3: "", 
             cell4: "", 
-            cell5: "X", 
+            cell5: "", 
             cell6: "", 
             cell7: "", 
-            cell8: "O", 
-            cell9: ""}
+            cell8: "", 
+            cell9: ""},
+            player : 1
     }
     this.handleClick = this.handleClick.bind(this);
+    this.clearBoard = this.clearBoard.bind(this);
 }
 
 handleClick(cellID){
-    this.setState(prevState => {
-        const newGameState = prevState.gameObject
-        newGameState[cellID] = "X"
-        return newGameState;
-    })  
+
+    if(this.state.player === 1) {
+        this.setState(prevState => {
+            const newState = prevState
+            newState.gameObject[cellID] = "X"
+            newState.player = 2
+            return newState;
+        })  
+    } else {
+        this.setState(prevState => {
+            const newState = prevState
+            newState.gameObject[cellID] = "O"
+            newState.player = 1
+            return newState;
+        })  
+    }
+
+    
 }
 
-// componentDidMount(){
-//     this.handleClick('cell9')
-// }
+clearBoard(){
+
+    this.setState(
+        {
+            gameObject: {
+                cell1: "", 
+                cell2: "",
+                cell3: "", 
+                cell4: "", 
+                cell5: "", 
+                cell6: "", 
+                cell7: "", 
+                cell8: "", 
+                cell9: ""},
+                player : 1
+        }
+
+    )
+
+}
 
 render(){
     
 return(
     <Fragment>
         <h1>Blub, Growl, Yum</h1>
+        <h2>Player {this.state.player}'s turn</h2>
         <GameContainer cellData={this.state.gameObject} handClick={this.handleClick}/>
+        <button onClick={this.clearBoard}>Clear</button>
     </Fragment>
 )
 }
