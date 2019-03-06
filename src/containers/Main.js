b258a4abdadf5d3a19f420c7f83ba97f7a4b359f
Rick Symington <rick.symington@gmail.com>
2019-03-06 14:10:50 +0000

    Add cellID

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,7 +8,7 @@ constructor(props){
     this.state = {
         gameObject: {
             cell1: "O", 
-            cell2: "O",
+            cell2: "X",
             cell3: "O", 
             cell4: "", 
             cell5: "X", 
@@ -17,9 +17,23 @@ constructor(props){
             cell8: "O", 
             cell9: ""}
     }
+    this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick(cellID){
+    this.setState(prevState => {
+        const newGameState = prevState.gameObject
+        newGameState[cellID] = "X"
+        return newGameState;
+    })  
+}
+
+componentDidMount(){
+    this.handleClick('cell9')
 }
 
 render(){
+    
 return(
     <Fragment>
         <h1>Blub, Growl, Yum</h1>
